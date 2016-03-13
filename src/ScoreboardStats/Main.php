f0d8ae6d74ba28888978ddfcf35bbb3c638a8ddf
NightmareCybers <fahmiahmadfadilah28@gmail.com>
2016-03-13 17:56:44 +0700

    Update Main.php

--- src/ScoreboardStats/Main.php
+++ src/ScoreboardStats/Main.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace ScoreboardStats;
+namespace CyberRealmsStats;
 
 use pocketmine\event\Listener;
 use pocketmine\plugin\PluginBase;
@@ -13,22 +13,22 @@ class Main extends PluginBase implements Listener {
  	public function onEnable() {
  		$this->getServer()->getPluginManager()->registerEvents($this, $this);
  		$this->EconomyS = $this->getServer()->getPluginManager()->getPlugin("EconomyAPI");
- 		$this->getServer()->getScheduler()->scheduleRepeatingTask(new CallbackTask(array($this, "ScoreboardStats")), 10);
-		$this->getLogger()->info("§a Enabled by TutoGamerWalid v1.0 !");
+ 		$this->getServer()->getScheduler()->scheduleRepeatingTask(new CallbackTask(array($this, "CyberRealmsStats")), 10);
+		$this->getLogger()->info("Â§a Enabled by TutoGamerWalid v1.0 !");
  		$this->timer = 0;
  	}
  
- 	public function ScoreboardStats() {
+ 	public function CyberRealmsStats() {
  		foreach($this->getServer()->getOnlinePlayers() as $players) {
  			$Name = $players->getPlayer()->getName();
  			$Money = $this->EconomyS->mymoney($Name);
  			$Online = count(Server::getInstance()->getOnlinePlayers());
  			$Full = $this->getServer()->getMaxPlayers();
- 			$players->sendTip(" §b« §1Cyber§2Realms§4Stats§b »\n §eHello:§b $Name !\n §aUang:§f $Money §2$\n §cOnline:§6 $Online §e/§6 $Full");
+ 			$players->sendTip(" Â§bÂ« Â§1CyberÂ§2RealmsÂ§4StatsÂ§b Â»\n Â§eHello:Â§b $Name !\n Â§aUang:Â§f $Money Â§2$\n Â§cOnline:Â§6 $Online Â§e/Â§6 $Full");
  		}
  	}
 
 	public function onDisable() {
-		$this->getLogger()->info("§4Disabled !");
+		$this->getLogger()->info("Â§4Disabled !");
 	}
 }
